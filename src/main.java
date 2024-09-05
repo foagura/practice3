b4ca7580d7161f00f414366a63a94e74fb291213
foagura <foagura_0416@yahoo.co.jp>
2024-09-05 09:06:26 +0900

    first commit

---
+++ src/main.java
@@ -0,0 +1,28 @@
+public class main {
+    public static void main(String[] args){
+        char[] a;
+        a = new char[3];
+        a[0] = 'q';
+        a[1] = 'w';
+        a[2] = 'e';
+
+        int []num = new int[3];
+        num[0] = 10;
+        num[1] = 20;
+        num[2] = 30;
+
+        String str[] = {"taro", "sige", "popo"};
+
+        char[] b = new char[6];
+        int[] num2 = new int[3];
+
+        System.out.println(a);
+        System.out.println(num[0]);
+        System.out.println(str[0]);
+        System.out.println(b);
+        System.out.println(num2[1]);
+        System.out.println(num);
+        System.out.println(str);
+        System.out.println(str.length);
+    }
+}
